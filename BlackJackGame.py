0cc23d2871661e3fc4a130a6220370ac35edfa40
Yoda <kirill.sidorov.98@mail.ru>
2020-02-09 14:03:14 +0100

    Working classical BlackJack

--- BlackJackGame.py
+++ BlackJackGame.py
@@ -3,31 +3,29 @@ from modules import Test
 deck = Test.Deck()
 dealer = Test.Player()
 player = Test.Player()
-game_on = True
 
-
-while game_on:
-    player.ask_name()
+print('Welcome to the new world of BlackJack!')
+player.ask_name()
+while True:
     deck.deck_creation()
-    while True:
-        player.betting()
-        for i in range(2):
-            card = Test.Card(deck.take_card())
-            player.print_card(card)
-            card = card.card_nominal()
-            player.get_card(card)
-            card = Test.Card(deck.take_card())
-            card = card.card_nominal()
-            dealer.get_card(card)
-        player.stand_or_hit(deck)
-        Test.win_bust_check(player, dealer, deck)
-        print(f"Dealer's hand is {dealer.hand}")
-        print(dealer.hand_score())
-        player.hand = list()
-        dealer.hand = list()
-        print(f"{player.name}'s bank is {player.bank}")
-        if not Test.replay():
-            break
-    print('Thanks for you time! See ya!')
-    break
+    player.betting()
+    for i in range(2):
+        card = Test.Card(deck.take_card())
+        player.print_card(card)
+        player.get_card(card, card.card_nominal())
+        card = Test.Card(deck.take_card())
+        dealer.get_card(card, card.card_nominal())
+    dealer.hide_card()
+    print(f"{player.name}'s hand value is {player.sumhand}")
+    player.stand_or_hit(deck)
+    Test.win_bust_check(player, dealer, deck)
+    print("Dealer's hand is "+', '.join([str(card) for card in dealer.hand]))
+    print(f"Dealer's value is {dealer.sumhand}")
+    player.hand = list()
+    dealer.hand = list()
+    print(f"{player.name}'s bank is {player.bank}")
+    if not Test.replay():
+        print('Thanks for your time! See ya!')
+        break
+
 
